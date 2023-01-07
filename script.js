9d3dab424c9d13a7899cb3d1bfdd72e6d3cf05b9
NineTen! <99680795+EuphoriaX2@users.noreply.github.com>
2023-01-06 22:24:12 -0600

    these files might get changed

---
+++ script.js
@@ -0,0 +1,21 @@
+function sign(input) {
+    document.write(input);
+};
+function spawn(input) {
+    switch (input) {
+        case "axolotl" :
+            document.write("spawned a axolotl");
+        break;
+        case "zombie" :
+            document.write("spawned a zombie");
+        break;
+        case "skeleton" :
+            document.write("spawned a skeleton");
+        break;
+    }
+}
+function credits() {
+    document.write("Made by nineten");
+    document.write("based of of minecraft")
+}
+
